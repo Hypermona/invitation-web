d4524abc0b67e1e33f8612325a9f92dfad872c8a
htihskay <pyaxith00024@gmail.com>
2023-08-29 23:51:59 +0530

    basic footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,40 @@
 import React from "react";
-
+import "./css/Footer.css"
 type Props = {};
 
 const Footer = (props: Props) => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+    <div className="footer-content">
+      <div className="footer-section">
+        <h3>About Us</h3>
+        <p>We are committed to providing great products and services.</p>
+      </div>
+      <div className="footer-section">
+        <h3>Contact Us</h3>
+        <p>Email: inbyte@xyz.com</p>
+        <p>Phone: +1-123-456-7890</p>
+      </div>
+      <div className="footer-section">
+        <h3>Follow Us</h3>
+        <div className="social-icons">
+          <a href="#">
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className="footer-bottom">
+      <p>&copy; 2023 Your Company. All rights reserved.</p>
+    </div>
+  </footer>
+  );
 };
 
 export default Footer;
