30f89f14de02749b55b661293e82859a3786873a
htihskay <pyaxith00024@gmail.com>
2023-08-30 22:32:18 +0530

    login/signup

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "./ui/button";
-
+import Link from 'next/link'
 type Props = {};
 
 const Header = (props: Props) => {
@@ -11,7 +11,8 @@ const Header = (props: Props) => {
         <div className="px-5 pt-2">Templates</div>
         <div className="px-5 pt-2">Plugins</div>
         <div className="px-5 pt-2">How to Use</div>
-        <Button className="px-5">Login</Button>
+         <Link href="/login"><Button className="px-5">Login</Button></Link>
+        
       </nav>
     </header>
   );
